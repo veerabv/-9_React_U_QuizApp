f999fd5f600d020520d8e094c57049323834d609
veerabv <bveera0608@gmail.com>
2024-07-15 20:34:29 +0530

    add quiz complete logic and suffling the answer everytime

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,37 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png"
 
 export default function Quiz() {
   //   const [activeQuestionIndex, setActiveQuestion] = useState(0);  //comment this state because we can derived this state from below state
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length; // this is the derived state from the above state
+  
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
+
   function handleSelectQuestion(answer) {
     setUserAnswers((previousUserAnswer) => {
       return [...previousUserAnswer, answer];
     });
   }
+  if (quizIsCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+  // i changed this line here from top to avoid the error in the suffledAnswers error , we can try by putting these line above the if statement
+  const suffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  suffledAnswers.sort(() => Math.random() - 0.5); // the sort function swap if the value is -1 or negative , so the random gives value 0 to 1 so i sub 0.5 to suffles the answers
+
   return (
-    <div id='quiz'>
+    <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {suffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectQuestion(answer)}>
                 {answer}
