eb27719f79cdb81bd9d562304f9cbf3500ae2c7d
veerabv <bveera0608@gmail.com>
2024-07-15 20:11:26 +0530

    showing the questions and awnser in the Quiz component and render that component in App

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,29 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestion] = useState(0);
+  //   const [activeQuestionIndex, setActiveQuestion] = useState(0);  //comment this state because we can derived this state from below state
   const [userAnswers, setUserAnswers] = useState([]);
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length; // this is the derived state from the above state
+  function handleSelectQuestion(answer) {
+    setUserAnswers((previousUserAnswer) => {
+      return [...previousUserAnswer, answer];
+    });
+  }
+  return (
+    <div id='quiz'>
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectQuestion(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
