3e70c842b4e9da54cf77dafa3dc38cdcb9b53181
veerabv <bveera0608@gmail.com>
2024-07-16 21:08:36 +0530

    moving the answer logic to Answer.jsx but delay to next question issue

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,37 +1,41 @@
-import { useState ,useCallback, useRef} from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../questions";
-import quizCompleteImg from "../assets/quiz-complete.png"
+import quizCompleteImg from "../assets/quiz-complete.png";
 import QuizTimer from "./QuizTimer";
+import Answers from "./Answers";
 
 export default function Quiz() {
-  const shuffledAnswers = useRef()
-  const [answerState , setAnswerState] = useState("")
+  const [answerState, setAnswerState] = useState("");
   //   const [activeQuestionIndex, setActiveQuestion] = useState(0);  //comment this state because we can derived this state from below state
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = answerState === "" ? userAnswers.length : userAnswers.length-1; // this is the derived state from the above state
-  
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1; // this is the derived state from the above state
+
   const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(answer) {
-    setAnswerState("answered")
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(answer) {
+    setAnswerState("answered");
     setUserAnswers((previousUserAnswer) => {
       return [...previousUserAnswer, answer];
     });
-    setTimeout(()=>{
-      if(answer === QUESTIONS[activeQuestionIndex].answers[0]){
-        setAnswerState("correct")
-      }else {
-        setAnswerState("wrong")
+    setTimeout(() => {
+      if (answer === QUESTIONS[activeQuestionIndex].answers[0]) {
+        setAnswerState("correct");
+      } else {
+        setAnswerState("wrong");
       }
-      setTimeout(()=>{
-        setAnswerState("")
-      },2000)
-      
-    },1000)
-  },[])
+      setTimeout(() => {
+        setAnswerState("");
+      }, 2000);
+    }, 1000);
+  }, []);
 
+  const handleSkipAnswer = useCallback(
 
-const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null) , [handleSelectAnswer])
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsCompleted) {
     return (
@@ -41,41 +45,24 @@ const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null) , [handleSelec
       </div>
     );
   }
-  if(!shuffledAnswers.current){
-    // i changed this line here from top to avoid the error in the suffledAnswers error , we can try by putting these line above the if statement
-   shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
-   shuffledAnswers.current.sort(() => Math.random() - 0.5); // the sort function swap if the value is -1 or negative , so the random gives value 0 to 1 so i sub 0.5 to suffles the answers
-
-  // the issue in the previous code is that we shuffle the answer for everytime the components rerendering .to avoid this we use ref 
-
-  }
-  
 
   return (
     <div id="quiz">
-
-      <QuizTimer key = {activeQuestionIndex} timeOut={15000} onTimeout={handleSkipAnswer}/>  {/*Key is not only used to render list the key prop is used to destroy the old element and create the new one */}
+      <QuizTimer
+        key={activeQuestionIndex}
+        timeOut={10000}
+        onTimeout={handleSkipAnswer}
+      />
+      {/*Key is not only used to render list the key prop is used to destroy the old element and create the new one */}
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.current.map((answer) => {
-            const isSelected = userAnswers[userAnswers.length -1] === answer;
-            let cssClass = '';
-            if(answerState === "answered" && isSelected){
-              cssClass="selected"
-            }
-            if((answerState === "correct" || answerState === "wrong") && isSelected){
-              cssClass = answerState;
-            }
-           return <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>
-              {answer}
-            </button>
-          </li>
-          }
-            
-          )}
-        </ul>
+        <Answers
+         key={activeQuestionIndex}
+          answers={QUESTIONS[activeQuestionIndex].answers}
+          selectedAnswer={userAnswers[userAnswers.length - 1]}
+          answerState = {answerState}
+          onSelect={handleSelectAnswer}
+        />
       </div>
     </div>
   );
