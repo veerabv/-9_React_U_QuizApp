33e342e9ccf38d94972c74223cc2137fc42e93ee
veerabv <bveera0608@gmail.com>
2024-07-16 20:08:40 +0530

    to solve the previouds issue we use useRef but new problem is the answer is not changed the same option is shown for all questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,10 @@
-import { useState ,useCallback} from "react";
+import { useState ,useCallback, useRef} from "react";
 import QUESTIONS from "../questions";
 import quizCompleteImg from "../assets/quiz-complete.png"
 import QuizTimer from "./QuizTimer";
 
 export default function Quiz() {
+  const shuffledAnswers = useRef()
   const [answerState , setAnswerState] = useState("")
   //   const [activeQuestionIndex, setActiveQuestion] = useState(0);  //comment this state because we can derived this state from below state
   const [userAnswers, setUserAnswers] = useState([]);
@@ -40,9 +41,15 @@ const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null) , [handleSelec
       </div>
     );
   }
-  // i changed this line here from top to avoid the error in the suffledAnswers error , we can try by putting these line above the if statement
-  const suffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  suffledAnswers.sort(() => Math.random() - 0.5); // the sort function swap if the value is -1 or negative , so the random gives value 0 to 1 so i sub 0.5 to suffles the answers
+  if(!shuffledAnswers.current){
+    // i changed this line here from top to avoid the error in the suffledAnswers error , we can try by putting these line above the if statement
+   shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+   shuffledAnswers.current.sort(() => Math.random() - 0.5); // the sort function swap if the value is -1 or negative , so the random gives value 0 to 1 so i sub 0.5 to suffles the answers
+
+  // the issue in the previous code is that we shuffle the answer for everytime the components rerendering .to avoid this we use ref 
+
+  }
+  
 
   return (
     <div id="quiz">
@@ -51,7 +58,7 @@ const handleSkipAnswer = useCallback(()=>handleSelectAnswer(null) , [handleSelec
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {suffledAnswers.map((answer) => {
+          {shuffledAnswers.current.map((answer) => {
             const isSelected = userAnswers[userAnswers.length -1] === answer;
             let cssClass = '';
             if(answerState === "answered" && isSelected){
